3425ed09d9df8f71f4d3fb887f52e87aa32a7a3e
Sneha Pal <118155232+SnehaPal16@users.noreply.github.com>
2025-08-17 21:32:32 +0530

    Question API DONE

---
+++ backend/controllers/questionController.js
@@ -0,0 +1,79 @@
+const Questions = require("../models/Questions");
+const Question = require("../models/Questions");
+const Session = require("../models/Session");
+
+exports.addQustionToSession = async(req , res) => {
+    try{
+        const {sessionId , questions} = req.body;
+
+        if(!sessionId || !questions || !Array.isArray(questions)){
+            return res.status(400).json({message : "Invalid Input Data"});
+        }
+
+        const session = await Session.findById(sessionId);
+
+        if(!session){
+            return res.status(404).json({message : "Session Not Found"});
+        }
+
+        // new question creation
+        const createQuestions = Question.insertMany(
+            questions.map((q) => ({
+                session: sessionId,
+                question: q.question,
+                answer: q.answer,
+            }))
+        )
+
+        // session ko update krna hai
+        session.questions.push(...createQuestions.map((q) => (q._id)));
+        await session.save();
+
+        res.status(201).json(createQuestions);
+
+    }
+    catch(error){
+        res.status(500).json({message : "Server Error"});
+    }
+};
+
+
+exports.togglePinQuestion = async(req , res) => {
+    try{
+        const question = await Question.findById(req.param.id);
+
+        if(!question){
+            return res.status(404).json({message : "Question Not Found"});
+        }
+
+        question.isPinned = !question.isPinned;
+        await question.save();
+
+        res.status(200).json({success : true , question})
+
+    }
+    catch(error){
+        res.status(500).json({message : "Server Error"});
+    }
+}
+
+
+
+exports.updateQustionNote = async(req , res) => {
+    try{
+        const {note} = req.body;
+        const question = await Question.findById(req.param.id);
+
+        if(!question){
+            return res.status(404).json({message : "Question Not Found"});
+        }
+
+        question.note = note || "";
+        await question.save();
+
+        res.status(200).json({success : true , question})
+    }
+    catch(error){
+        res.status(500).json({message : "Server Error"});
+    }
+}
