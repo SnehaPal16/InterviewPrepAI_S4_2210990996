7958e272eabd908e3aac63e486028640f4fc021c
Sneha Pal <118155232+SnehaPal16@users.noreply.github.com>
2025-08-18 09:44:58 +0530

    AI_APIs_Done

--- backend/controllers/aiController.js
+++ backend/controllers/aiController.js
@@ -1,5 +1,5 @@
 const {GoogleGenAI} = require("@google/genai");
-const {conceptExplainPrompt} = require('../utils/prompts');
+const {questionAnswerPrompt , conceptExplainPrompt} = require('../utils/prompts');
 
 const ai = new GoogleGenAI({ apiKey : process.env.GEMINI_API_KEY});
 
@@ -14,16 +14,52 @@ const generateInterviewQuestions = async(req , res) =>{
         const prompt = questionAnswerPrompt(role , experience , topicToFocus , numberOfQuestions);
 
         const response = await ai.models.generateContent({
-            
+            model: "gemini-2.0-flash-lite",
+            contents: prompt,
         })
+
+        let rawText = response.text;
+
+        const cleanedText = rawText.replace(/^```json\s*/ , "").replace(/```$/ , "").trim();
+
+        const data = JSON.parse(cleanedText);
+
+        res.status(200).json(data);
     }
     catch(error){
         res.status(500).json({ message : "Failed To Generate Question" , error : error.message});
     }
 }
 
+
+
 const generateInterviewExplanation = async(req , res) =>{
-    
+    try{
+        const {question} = req.body;
+
+        if(!question){
+            return res.status(400).json({message : "Missing Required Feilds"});
+        }
+
+        const prompt = conceptExplainPrompt(question);
+
+        const response = await ai.models.generateContent({
+            model: "gemini-2.0-flash-lite",
+            contents: prompt,
+        })
+
+        let rawText = response.text;
+
+        const cleanedText = rawText.replace(/^```json\s*/ , "").replace(/```$/ , "").trim();
+
+        const data = JSON.parse(cleanedText);
+
+        res.status(200).json(data);
+        
+    }
+    catch(error){
+        res.status(500).json({ message : "Failed To Generate Explanation" , error : error.message});
+    }
 }
 
 
