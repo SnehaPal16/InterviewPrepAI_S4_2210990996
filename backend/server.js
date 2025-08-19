b818f76e79af68e21ec7e124ef7f53cc7fa7256c
Shivansh <shivansh832.be22@chitkara.edu.in>
2025-08-19 14:07:58 +0530

    optimized

--- backend/server.js
+++ backend/server.js
@@ -10,25 +10,25 @@ const {protect} = require("./middlewares/authMiddleware");
 const {generateInterviewQuestions , generateInterviewExplanation} = require("./controllers/aiController");
 
 const app = express();
-connectDB();
 
 // Middleware
 app.use(express.json());
+
+//ROUTES
 app.use('/api/auth' , authRoutes);
-// app.use('/api/sessions' , sessionRoutes);
+app.use('/api/sessions' , sessionRoutes);
 app.use('/api/questions' , questionRoutes);
 
 app.use("/api/ai/generate-questions", protect , generateInterviewQuestions);
 app.use("/api/ai/generate-explanations", protect , generateInterviewExplanation);
-
-
-// Routes
-app.use('/api/questions' , questionRoutes);
-app.use('/api/sessions' , sessionRoutes);
 
 // Serve uploads folder
 app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
 // Start Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log("Server running on port: ", PORT);
+  });
+});
